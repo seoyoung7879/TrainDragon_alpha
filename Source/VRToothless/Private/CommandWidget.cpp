69c43a66955129a6a3d5dd83a5a13fee93af68bf
seoyoung7879 <hg2263@naver.com>
2024-05-21 15:54:29 +0900

    ㅂ

---
+++ Source/VRToothless/Private/CommandWidget.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CommandWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "DartCharacter.h"
+#include "EngineUtils.h"
+
+
+
+
+
+void UCommandWidget::NativeConstruct()
+{
+	for (TActorIterator<ADartCharacter> it(GetWorld()); it; ++it)
+	{
+		dragon = *it;
+	}
+
+	btn_sit->OnClicked.AddDynamic(this, &UCommandWidget::Sit);
+	btn_bang->OnClicked.AddDynamic(this, &UCommandWidget::Bang);
+	btn_fly->OnClicked.AddDynamic(this, &UCommandWidget::Fly);
+	btn_come->OnClicked.AddDynamic(this, &UCommandWidget::Comeon);
+	btn_return->OnClicked.AddDynamic(this, &UCommandWidget::ReturnMenu);
+
+}
+
+void UCommandWidget::Sit()
+{
+	dragon->SitAnim();
+	UE_LOG(LogTemp, Warning, TEXT("1"));
+}
+
+void UCommandWidget::Fly()
+{
+	dragon->FlyAnim();
+	UE_LOG(LogTemp, Warning, TEXT("2"));
+}
+
+void UCommandWidget::Bang()
+{
+	dragon->BangAnim();
+	UE_LOG(LogTemp, Warning, TEXT("3"));
+}
+
+void UCommandWidget::Comeon()
+{
+	UE_LOG(LogTemp, Warning, TEXT("5"));
+}
+
+void UCommandWidget::ReturnMenu()
+{
+	UE_LOG(LogTemp, Warning, TEXT("4"));
+}
